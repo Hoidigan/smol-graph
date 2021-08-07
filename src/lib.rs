254d7e5f1cb3e27600871049355068fb5b6eca51
Daniel Nelsen <Hoidigan@users.noreply.github.com>
2021-08-06 17:32:33 -0700

    update docs and readme for links

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,15 @@
-#[doc = include_str!("../README.md")]
+//! # smol-graph
+//!
+//! A bare bones graph implementaion for when you just want
+//! a graph with no hassle.
+//! 
+//! It uses [`uuid::Uuid`](uuid::Uuid) for its indices (inside newtypes).
+//! This means it is possible to generate an id that already exists,
+//! but the chance is essentially 0.
+//! 
+//! This crate also provides a [`prelude`](prelude) that exports all of the
+//! types used in this crate: [`Graph`](Graph), [`NodeIndex`](NodeIndex), 
+//! and[`EdgeIndex`](EdgeIndex).
 
 use std::collections::HashMap;
 
