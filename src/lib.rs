3f4a97af672660fe48c9c5bac8d347a3ffbbc300
Daniel Nelsen <Hoidigan@users.noreply.github.com>
2021-08-06 17:11:46 -0700

    Add README

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,4 @@
-//! A bare bones graph implementaion for when you just want
-//! a graph with no hassle.
-//!
-//! It uses [`uuid::Uuid`](uuid::Uuid) for its indices (inside newtypes).
-//! This means it is possible to generate an id that already exists,
-//! but the chance is essentially 0.
-//! 
-//! This crate also provides a [`prelude`] that exports all of the
-//! types used in this crate: [`Graph`], [`NodeIndex`], [`EdgeIndex`].
-//! 
+#[doc = include_str!("../README.md")]
 
 use std::collections::HashMap;
 
